0fb0b966984c469198d2729f035ae1b656642dc4
sedat <somamebarge@gmail.com>
2023-08-25 23:06:49 +0200

    Homework

---
+++ src/main/java/day03methodobjectcreationscanner/TeacherHomework.java
@@ -0,0 +1,19 @@
+package day03methodobjectcreationscanner;
+
+public class TeacherHomework {
+    /*
+     Homework
+     3) Icinde isim ve yas variable olan teach method u bulunan bir Teacher object olusturun ve obje uzerinde bu özellikleri kullanin
+     */
+
+public String name = "Benjamin";
+public byte age = 24;
+
+public void teach (){
+
+        System.out.println("Ogretmen cok iyi bir anlatici.");
+
+        //Run işlemi Runner class inda yapilmistir.
+
+        }
+        }
