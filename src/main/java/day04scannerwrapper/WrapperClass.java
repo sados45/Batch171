2edcd70905f1241dbe3f64f0785442329caaa577
sedat <somamebarge@gmail.com>
2023-08-25 23:13:03 +0200

    WrapperClass

--- src/main/java/day04scannerwrapper/WrapperClass.java
+++ src/main/java/day04scannerwrapper/WrapperClass.java
@@ -25,5 +25,44 @@ public class WrapperClass {
         short minShort = Short.MIN_VALUE;
         System.out.println("minShort = " + minShort);//minShort = -32768
 
+
+
+
+
+        //Ornek 2 : int data type ini minimum degeri ile byte data type maximum degerlerinin toplamini bulunuz.
+
+        int minInt = Integer.MIN_VALUE;
+
+        byte maxByte = Byte.MAX_VALUE;
+
+        int addition = minInt + maxByte;
+
+        System.out.println("addition = " + addition);
+
+        //Ornek 3 : Primitive int i wrapper Integer a ceviriniz.
+
+        int num = 12;
+
+        Integer wrapperNum = num; //Autoboxing
+
+        //Ornek 4 : Wrapper byte i primitive byte a ceviriniz.
+
+        Byte wrapperByte = 20;
+
+        byte num2 = wrapperByte; //Unboxing
+
+        //Ornek 5 : Primitive char i Wrapper Character e donusturunuz.
+
+        char letter = 'a';
+
+        Character wrapperLetter = letter;
+
+        //Ornek 6 : Wrapper boolean i primitive boolean a ceviriniz.
+
+        Boolean wrapperQuestion = false;
+
+        boolean question = wrapperQuestion;
+
     }
-}
+
+    }
