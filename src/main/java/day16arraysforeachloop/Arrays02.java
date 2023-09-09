6496022acd1a503d1cee4c27893ecb7a1ead8351
sedat <somamebarge@gmail.com>
2023-09-09 23:22:38 +0200

    Arrays

--- src/main/java/day16arraysforeachloop/Arrays02.java
+++ src/main/java/day16arraysforeachloop/Arrays02.java
@@ -1,4 +1,93 @@
 package day16arraysforeachloop;
 
+import java.util.Arrays;
+
 public class Arrays02 {
+
+    public static void main(String[] args) {
+
+        // Ornek 1 : String array olusturun, icine 5 tane eleman ekleyin, ilk eleman ile son elemanin icerdigi karakter sayilari toplamini yazidiriniz
+
+        String arr [] = new String[5];
+
+        arr [0] = "Math";
+        arr [1] = "Science";
+        arr [2] = "Music";
+        arr [3] = "English";
+        arr [4] = "Art";
+
+        Arrays.toString(arr);
+
+        int firstLength = arr [0].length();
+        int lastLength = arr [arr.length-1].length();
+
+        System.out.println(firstLength + lastLength);
+
+        // Ornek String bir array olusturun icine 5 tane eleman ekleyin, tum elemanlarin icerdigi karakter sayilarinin toplamini yazdiriniz
+
+        String cities [] = new String [5];
+
+        cities [0] = "Istanbul";
+        cities [1] = "Miami";
+        cities [2] = "Ankara";
+        cities [3] = "Oslo";
+        cities [4] = "Erzurum";
+
+        // 1. Yol for loop
+
+        int totalChar = 0;
+
+        for (int i = 0; i < cities.length ; i++) {
+
+            totalChar += cities[i].length();
+
+        }
+
+        System.out.println(totalChar);
+
+        // 2. Yol for each loop (enhanced loop --> zenginleştirilmiş loop)
+        // baslangic degeri, loopun calisma sarti ve increment/decrement kismini kendisi halleder.
+        // for each loop "Array" lerde ve "Collection" larda kullanilir.
+
+        /*
+        for (DataType w --> (variable name) : arr/collection) {
+            calisacak kodlar
+        }
+        */
+
+        int sum = 0;
+
+        for (String w : cities) {
+
+            sum += w.length();
+
+        }
+
+        System.out.println(sum);
+
+        // Ornek 3 : Notlar adinda bir Integer array olusturunuz. Icıne 6 tane not yerlestiriniz ve not ort. ekrana aliniz.
+
+        int notes [] = new int[6];
+
+        notes [0] = 50;
+        notes [1] = 70;
+        notes [2] = 60;
+        notes [3] = 40;
+        notes [4] = 90;
+        notes [5] = 80;
+
+        System.out.println(Arrays.toString(notes));
+
+        int addition = 0;
+
+        for (int w : notes) {
+
+            addition += w;
+
+        }
+
+        System.out.println(addition / notes.length);
+
+    }
+
 }
--- src/main/java/day16arraysforeachloop/Arrays03.java
+++ src/main/java/day16arraysforeachloop/Arrays03.java
@@ -1,4 +1,79 @@
 package day16arraysforeachloop;
 
+import java.util.Arrays;
+
 public class Arrays03 {
+
+    public static void main(String[] args) {
+
+        // Ornek 1 : Integer bir array olusturunuz, icine 6 eleman yerlestiriniz. Bu elemanlarin en kucugu ile en buyugunun toplamini ekrana yazdiriniz.
+
+        int ages [] = new int[6];
+
+        ages [0] = 20;
+        ages [1] = 23;
+        ages [2] = 19;
+        ages [3] = 44;
+        ages [4] = 15;
+        ages [5] = 32;
+
+        System.out.println(Arrays.toString(ages));
+
+        // 1. Yol
+        // sort method u array deki elemanlari kucukten buyuge dizer.
+
+        Arrays.sort(ages); // kucukten buyuge siralama.
+
+        System.out.println(Arrays.toString(ages));
+
+        System.out.println(ages [0] + ages[ages.length - 1]);
+
+        // 2. Yol
+
+        int minimum = ages[0];
+        int maximum = ages[0];
+
+        for (int w : ages) {
+
+            minimum = Math.min(minimum , w);
+
+            maximum = Math.max(maximum , w);
+
+        }
+
+        System.out.println(minimum);
+
+        System.out.println(maximum);
+
+        System.out.println("En buyuk sayi = " + maximum +"\nEn kucuk sayi = " +minimum);
+
+        // Ornek 2 : String bir array olusturunuz 6 eleman ekleyiniz, yellow dan onceki elemalari yazdiriniz.
+
+        String colours[] = new String [6];
+
+        colours [0] = "Red";
+        colours [1] = "Orange";
+        colours [2] = "Blue";
+        colours [3] = "Yellow";
+        colours [4] = "Green";
+        colours [5] = "Brown";
+
+        System.out.println(Arrays.toString(colours));
+
+        for (String w : colours) {
+
+            if (w.equals("Yellow")){
+
+                break;
+
+            }
+
+            System.out.println(w);
+
+        }
+
+
+
+    }
+
 }
