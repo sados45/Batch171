ef5ca4ba5e925264e401888df70d1fa1397de2c0
sedat <somamebarge@gmail.com>
2023-09-09 23:19:56 +0200

    NestedLoop

--- src/main/java/day15loops/NestedLoop.java
+++ src/main/java/day15loops/NestedLoop.java
@@ -1,28 +1,72 @@
 package day15loops;
 import java.util.Scanner;
+
 public class NestedLoop {
+
     public static void main(String[] args) {
+
         /*
         Example 1: Asagidaki sekli cizen kodu yaziniz
                     1
                     1 2
                     1 2 3
                     1 2 3 4
-                    1 2 3 4 5               */
+                    1 2 3 4 5
+        */
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Satir (row) sayisini giriniz..");
+
+        System.out.println("Satir (row ) sayisini giriniz...");
+
         int row = input.nextInt();
 
-        for (int i = 1; i <= row;                i++) {
+        for (int i = 1 ; i <= row ; i++) {
 
-            for (int k = 1; k <=i ;              k++) {
-                System.out.print(k+" ");
+            for (int j = 1 ; j <= i ; j++) {
+
+                System.out.print(j + " ");
 
             }
+
             System.out.println();
+
         }
 
+        // Ornek 2 : 23 den 12 ye kadar cift tam sayilari console a yazdiriniz.
+
+        int j = 23;
+
+        while (j > 11) {
+
+            if (j % 2 == 0) {
+
+                System.out.print(j + " ");
+
+            }
+
+            j--;
+
+        }
+
+        System.out.println();
+
+        // Ornek 3 : 6 dan 19 a kadar tek tamsayilari console a yazdriniz.
+
+        int k = 6;
+
+        while(k < 20) {
+
+            if (k % 2 != 0) {
+
+                System.out.print(k + " ");
+
+            }
+
+            k++;
+
+        }
+
+        System.out.println();
 
     }
 
